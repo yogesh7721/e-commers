6f78281b690a6fb75b7b3e0413a7ced8978590b7
yogesh <shelkeyogesh874@gmail.com>
2024-07-08 15:33:49 +0530

    server update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.static("uploads"))
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: true,
     credentials: true
 }))
 
